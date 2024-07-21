b65bb6b86a46d4a2b1adee06aa56d1bc2e82e23b
wangshiyu <295846328@qq.com>
2024-07-21 21:08:08 +0800

    习惯配置添加

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vant from 'vant' // Vant组件库加载
 import './styles/global.less'
-Vue.config.productionTip = false
+import 'vant/lib/index.css' // 全局样式加载
+import 'amfe-flexible' // 自动设置rem基准值
 
+Vue.config.productionTip = false
+Vue.use(Vant) // 全局注册Vant组件
 // 创建vue实例，将router、store配置到根实例中
 // 把APP根组件渲染到#app节点中
 new Vue({
---
+++ src/views/login/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>sas</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'loginIndex',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.ss {
+  font-size: 12px;
+}
+</style>
